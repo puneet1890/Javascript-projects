872771fbc7bbc65c667f202e9641010a307edc8b
Puneet L <puneet1890@gmail.com>
2025-07-05 15:06:31 +0530

    First JS project

---
+++ 01_Project/app.js
@@ -0,0 +1,13 @@
+console.log("Starting my first Javascript application...");
+
+let n = 5;
+let m = 10;
+
+var result = sumOfNumbers(m, n);
+console.log("The sum of two nos: "+result);
+
+function sumOfNumbers(n1, n2) { 
+  return n1 +n2;
+}
+
+console.log("Application finished.");
